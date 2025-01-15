d3dbb3f8d14959dab6b8354a74f876e39548376b
LihanChen2004 <757003373@qq.com>
2025-01-15 17:33:06 +0800

    🎈 perf(rm_sentry_state_publisher_launch.py): Simplify launch file to include robot description

--- pb2025_nav_bringup/launch/rm_sentry_state_publisher_launch.py
+++ pb2025_nav_bringup/launch/rm_sentry_state_publisher_launch.py
@@ -21,12 +21,11 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription
 from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node, PushRosNamespace
-from sdformat_tools.urdf_generator import UrdfGenerator
-from xmacro.xmacro4sdf import XMLMacro4sdf
+from launch_ros.actions import PushRosNamespace
 
 
 def generate_launch_description():
@@ -38,21 +37,7 @@ def generate_launch_description():
     namespace = LaunchConfiguration("namespace")
     use_namespace = LaunchConfiguration("use_namespace")
     use_sim_time = LaunchConfiguration("use_sim_time")
-    # xmacro_description = LaunchConfiguration("xmacro_description")
-    xmacro_description = os.path.join(
-        pkg_pb2025_robot_description_dir,
-        "resource",
-        "xmacro",
-        "pb2025_sentry_robot.sdf.xmacro",
-    )
-
-    # Map fully qualified names to relative ones so the node's namespace can be prepended.
-    # In case of the transforms (tf), currently, there doesn't seem to be a better alternative
-    # https://github.com/ros/geometry2/issues/32
-    # https://github.com/ros/robot_state_publisher/pull/30
-    # TODO(orduno) Substitute with `PushNodeRemapping`
-    #              https://github.com/ros2/launch_ros/issues/56
-    remappings = [("/tf", "tf"), ("/tf_static", "tf_static")]
+    robot_name = LaunchConfiguration("robot_name")
 
     # Declare the launch arguments
     declare_namespace_cmd = DeclareLaunchArgument(
@@ -73,39 +58,27 @@ def generate_launch_description():
         description="Use simulation (Gazebo) clock if true",
     )
 
-    xmacro = XMLMacro4sdf()
-    xmacro.set_xml_file(xmacro_description)
-
-    # Generate SDF from xmacro
-    xmacro.generate()
-    robot_xml = xmacro.to_string()
-
-    # Generate URDF from SDF
-    urdf_generator = UrdfGenerator()
-    urdf_generator.parse_from_sdf_string(robot_xml)
-    robot_urdf_xml = urdf_generator.to_string()
+    declare_robot_name_cmd = DeclareLaunchArgument(
+        "robot_name",
+        default_value="pb2025_sentry_robot",
+        description="The file name of the robot xmacro to be used",
+    )
 
     bringup_cmd_group = GroupAction(
         [
             PushRosNamespace(condition=IfCondition(use_namespace), namespace=namespace),
-            Node(
-                package="joint_state_publisher",
-                executable="joint_state_publisher",
-                name="joint_state_publisher",
-                output="screen",
-                remappings=remappings,
-            ),
-            Node(
-                package="robot_state_publisher",
-                executable="robot_state_publisher",
-                name="robot_state_publisher",
-                remappings=remappings,
-                parameters=[
-                    {
-                        "use_sim_time": use_sim_time,
-                        "robot_description": robot_urdf_xml,
-                    }
-                ],
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(
+                        pkg_pb2025_robot_description_dir,
+                        "launch",
+                        "robot_description_launch.py",
+                    )
+                ),
+                launch_arguments={
+                    "use_sim_time": use_sim_time,
+                    "robot_name": robot_name,
+                }.items(),
             ),
         ]
     )
@@ -117,6 +90,7 @@ def generate_launch_description():
     ld.add_action(declare_namespace_cmd)
     ld.add_action(declare_use_namespace_cmd)
     ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_robot_name_cmd)
 
     # Add the actions to launch all nodes
     ld.add_action(bringup_cmd_group)
